535bb8bd3db3b11ad96b73f3083bdc8ba781055f
Hakim Balogun <addhak@gmail.com>
2023-02-23 07:56:25 -0800

    Fix error in code file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,15 +12,16 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
-		} else
-		{
-			n1 = n;
-		}
+	}
+	else
+	{
+		n1 = n;
+	}
 
-		if (n1 / 10)
-		{
-			print_number(n1 / 10);
-		}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
 
-		_putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
